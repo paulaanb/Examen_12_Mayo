d217cb676cced1a54cb60f9f7c4e4df873121e72
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-05-12 08:58:07 +0200

    archivo navegacion

--- Clases/codigo.py
+++ Clases/codigo.py
@@ -14,3 +14,12 @@ decrypted = fernet.decrypt(encrypted)
   
 with open('conversiones (4).csv', 'wb') as dec_file: 
     dec_file.write(decrypted) 
+
+
+with open('navegacion (4).csv', 'rb') as enc_file: 
+    encrypted = enc_file.read() 
+  
+decrypted = fernet.decrypt(encrypted) 
+  
+with open('navegacion (4).csv', 'wb') as dec_file: 
+    dec_file.write(decrypted) 
