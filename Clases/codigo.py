41661e100b89362b8a3cc8ea5646faddfb11ff57
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-05-12 07:07:28 +0000

    creamos el código para poder desencreptar

--- Clases/codigo.py
+++ Clases/codigo.py
@@ -1,4 +1,3 @@
-pip instalar criptografía
 from cryptography.fernet import Fernet
 key = Fernet.generate_key() 
   
@@ -13,4 +12,12 @@ with open('conversiones (4).csv', 'rb') as enc_file:
 decrypted = fernet.decrypt(encrypted) 
   
 with open('conversiones (4).csv', 'wb') as dec_file: 
+    dec_file.write(decrypted) 
+
+with open('navegacion (4).csv', 'rb') as enc_file: 
+    encrypted = enc_file.read() 
+  
+decrypted = fernet.decrypt(encrypted) 
+  
+with open('navegacion (4).csv', 'wb') as dec_file: 
     dec_file.write(decrypted) 
