9eb99c019b82d166902b43158d6716c8d4cf603f
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-05-12 08:56:58 +0200

    intento de desencriptar el archivo

---
+++ Clases/codigo.py
@@ -0,0 +1,16 @@
+pip instalar criptografía
+from cryptography.fernet import Fernet
+key = Fernet.generate_key() 
+  
+with open('filekey.key', 'wb') as filekey: 
+   filekey.write(key)
+
+fernet = Fernet(key) 
+  
+with open('conversiones (4).csv', 'rb') as enc_file: 
+    encrypted = enc_file.read() 
+  
+decrypted = fernet.decrypt(encrypted) 
+  
+with open('conversiones (4).csv', 'wb') as dec_file: 
+    dec_file.write(decrypted) 
