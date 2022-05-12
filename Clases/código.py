c5c8f4486c1ec3593fcbeaddf8bed4c0c2d4ab2c
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-05-12 07:51:19 +0000

    importamos librerías

---
+++ Clases/código.py
@@ -0,0 +1,8 @@
+#Importamos las librerias que tenemos que utilizar
+import re
+from numpy import shares_memory
+from numpy.lib.function_base import append
+import pandas as pd
+from pandas import tseries
+
+
